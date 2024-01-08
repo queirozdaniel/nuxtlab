4f30f8ba722b58ab9031fa365c65f59ba950aad7
queirozdaniel <danielqueirozak77@gmail.com>
2024-01-07 23:57:11 -0300

    chore: Configure Tailwind in project

--- packages/ui-layer/components/HelloWorld.vue
+++ packages/ui-layer/components/HelloWorld.vue
@@ -3,8 +3,11 @@ const { layer } = useAppConfig()
 </script>
 
 <template>
-  <div>
-    <h1>Hello World!</h1>
-    <pre>{{ layer }}</pre>
+  <div class="bg-primary-300">
+    <h1>Hello World !!</h1>
+    <h1 class="text-3xl font-bold p-4 text-primary-500"  title="heading 1"> 
+      {{ layer.name }}
+    </h1>
+    <!-- <pre>{{ layer }}</pre> -->
   </div>
 </template>
