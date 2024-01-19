44e30b2b06958d87a317521be61e833e587e99aa
queirozdaniel <danielqueirozak77@gmail.com>
2024-01-18 23:06:00 -0300

    feat: adding inputs without controls

---
+++ packages/ui-layer/components/forms/input-label-help.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+    <div class="mt-1">
+      <input type="text" name="email" id="email" class="shadow-sm px-3 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="you@example.com" aria-describedby="email-description" />
+    </div>
+    <p class="mt-2 text-sm text-gray-500" id="email-description">We'll only use this for spam.</p>
+  </div>
+</template>
