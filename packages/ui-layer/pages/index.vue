017494d192b2b794ec4c91830b1cfceeb5a316f8
queirozdaniel <danielqueirozak77@gmail.com>
2024-01-18 22:24:00 -0300

    feat: adding componets to ui

---
+++ packages/ui-layer/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="text-2xl">Exemplos</h1>
+    <div class="flex my-4 gap-4">
+        <preview-component title="Input Add">
+            <forms-input-add> </forms-input-add>
+        </preview-component>
+    </div>
+</template>
